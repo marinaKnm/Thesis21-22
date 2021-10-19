e84290a510defc5d843436fb9fca9a750a538ce9
annalogotheti <annalogotheti103@gmail.com>
2021-10-19 21:08:35 +0300

    myMessage

--- functions.js
+++ functions.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){   //make sure the document is already loaded
 
+  var canvas = d3.select("body")
+               .append("svg") //so we append the svg element to our page
+               //now let's give some properties
+               .attr("width", 725) //when we are styling svg elements, we use attr method
+               .attr("height", 500) //instead of style method
+               .style("margin-left","400px")
+               .style("background-color","#e4dada")
+               .style("border","solid 3px");
+  //Let's start by creating a circle:
+   var circle1 = canvas.append("circle")
+                .attr("cx", 250) //we want to give it a horizontal position
+                .attr("cy", 250)
+                .attr("r", 150) //radius
+                .attr("stroke","green")
+                .attr("stroke-width",3)
+                .attr("fill","none");
+
+   var circle2 = canvas.append("circle")
+                .attr("cx", 450) //we want to give it a horizontal position
+                .attr("cy", 250)
+                .attr("r", 150) //radius
+                .attr("stroke","orange")
+                .attr("stroke-width",3)
+                .attr("fill","none");
+
   // when you hover over a button change colour to pink
   $(".Button").hover(function() {
     $(this).css("background-color", "pink");
@@ -60,7 +85,7 @@ $(document).ready(function(){   //make sure the document is already loaded
   $('#Submit').click(function() {
     let inputStr = $('.text-box').html();      //get the text from the text-box
 
-    //if input starts with union or intersection or ' or ) accordingly show error message 
+    //if input starts with union or intersection or ' or ) accordingly show error message
     switch (inputStr[0]) {
       case '\u222A':
         showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');
@@ -72,7 +97,7 @@ $(document).ready(function(){   //make sure the document is already loaded
         showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');
         return;
       case ')':
-        showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');    
+        showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');
         return;
     }
 
@@ -99,7 +124,7 @@ $(document).ready(function(){   //make sure the document is already loaded
         showErrorMessage("Μη έγκυρη πρόταση. Δόθηκε " +prev+ " , αναμένεται \u222A, \u2229, '");
         return;
       }
- 
+
       //make sure that left parethesis and right parenthesis are balanced and paired
       if (inputStr[i] === '(') {
         stack = stack.concat(stack, '(');  //push left parenthesis into the stack
@@ -115,6 +140,10 @@ $(document).ready(function(){   //make sure the document is already loaded
       showErrorMessage('Μη έγκυρη πρόταση. Οι παρενθέσεις δεν είναι ισοζυγισμένες.');
     }
 
+    //Now we'll visualize user's set in the Venn diagram
+    //d3.select('#set'+i).style('fill',fill);
+    circle1.attr("fill","pink");
+    circle2.attr("fill","pink");
   });
 
 });
