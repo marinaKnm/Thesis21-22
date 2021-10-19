ecff474177516ac1b411756701585b65fa39b7bc
marinaKnm <konomimarina830@gmail.com>
2021-10-19 16:45:47 +0300

    error checking

--- functions.js
+++ functions.js
@@ -1,44 +1,115 @@
-$(document).ready(function(){
+$(document).ready(function(){   //make sure the document is already loaded
 
- $(".Button").hover(function() {
-  $(this).css("background-color", "pink");
-  },function(){
-    $(this).css("background-color", "#f4f4f4");
+  // when you hover over a button change colour to pink
+  $(".Button").hover(function() {
+    $(this).css("background-color", "pink");
+    },function(){
+      $(this).css("background-color", "lightslategray");
+    });
+
+  // functionality for each button
+  $('#A').click(function() {
+    $("#set").append("A");      //show in the text-box
   });
 
- $('#A').click(function() {
-   $("#set").append("A");
- });
- $('#B').click(function() {
-   $("#set").append("B");
- });
- $('#Union').click(function() {
-  $('#set').append('&cup;');
- });
- $('#Intersection').click(function() {
-  $('#set').append('&cap;');
- });
- $('#Compl').click(function() {
-   $("#set").append("'");
- });
- $('#leftPar').click(function() {
-   $("#set").append("(");
- });
- $('#rightPar').click(function() {
-   $("#set").append(")");
- });
- $('#EmptySet').click(function() {
-   $('#set').append('&empty;');
- });
+  $('#B').click(function() {
+    $("#set").append("B");      //show in the text-box
+  });
 
- $('#Delete').click(function(index, value) {
-   var myInput = $('.text-box').html();
-   myInput = myInput.substring(0, myInput.length - 1);
-   $('#set').text(myInput);
- });
+  $('#Union').click(function() {
+    $('#set').append('&cup;');      //show in the text-box
+  });
 
- $('#Reset').click(function() {
-   $('.text-box').empty();
- });
+  $('#Intersection').click(function() {
+    $('#set').append('&cap;');      //show in the text-box
+  });
+
+  $('#Compl').click(function() {
+    $("#set").append("'");      //show in the text-box
+  });
+
+  $('#leftPar').click(function() {
+    $("#set").append("(");      //show in the text-box
+  });
+
+  $('#rightPar').click(function() {
+    $("#set").append(")");    //show in the text-box
+  });
+
+  $('#EmptySet').click(function() {
+    $('#set').append('&empty;');    //show in the text-box
+  });
+
+  $('#Delete').click(function(index, value) {
+    let myInput = $('.text-box').html();      //get the text from the text-box
+    myInput = myInput.substring(0, myInput.length - 1);     //remove the last character
+    $('#set').text(myInput);      //show in the text-box
+  });
+
+  $('#Reset').click(function() {
+    $('.text-box').empty();     //delete the string
+  });
+
+  function showErrorMessage(str) {
+    $('.error_msg').html(str);
+    $('.error_msg').fadeIn(3000);
+    $('.error_msg').fadeOut(3000);
+  }
+var cnt = 0;  ////////////////
+  //make sure this is not an ivalid input
+  $('#Submit').click(function() {
+    let inputStr = $('.text-box').html();      //get the text from the text-box
+    cnt++;  //////////////////////
+    //if input starts with union or intersection or ' or ) accordingly show error message 
+    switch (inputStr[0]) {
+      case '\u222A':
+        showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');
+        break;
+      case '\u2229':
+        showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');
+        break;
+      case "'":
+        showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');
+        break;
+      case ')':
+        showErrorMessage('Μη έγκυρη πρόταση. Πρέπει να ξεκινάει με σύνολο ή αριστερή παρένθεση.');    
+        break;
+      default:
+        console.log('Default action'); 
+    }
+
+    let prev="";
+    let stack = "";
+    console.log(inputStr);
+    for(let i=0; i<inputStr.length; i++) {      //check character by character
+
+      //make sure that 2 sets are not given consequently
+      if ((prev === 'A' || prev === 'B') && (inputStr[i] === 'A' || inputStr[i] === 'B')) {
+        showErrorMessage('Μη έγκυρη πρόταση. Δόθηκαν δύο σύνολα στη σειρά.');
+      }
+
+      if ((prev === '\u222A' || prev === '\u2229') && (inputStr[i] === '\u2229' || inputStr[i] === '\u222A')) {
+        showErrorMessage('Μη έγκυρη πρόταση. Δόθηκε ' +prev+ ' και αναμένεται A, Β, (, ' + '\u2205');
+      }
+      // console.log('Character is: ' + inputStr[i]);
+      //make sure that left parethesis and right parenthesis are balanced and paired
+      if (inputStr[i] === '(') {
+        stack = stack.concat(stack, '(');  //push left parenthesis into the stack
+        console.log(stack);
+      }
+      if(inputStr[i] === ')') {
+        stack = stack.substring(0, stack.length - 1); //if right parenthesis is found pop from stack
+      }
+
+      prev = inputStr[i];
+    }
+
+    // console.log(stack);
+    if(stack !== "") {  //if stack is not empty then left parethesis and right parenthesis are not balanced and paired
+      showErrorMessage('Μη έγκυρη πρόταση. Οι παρενθέσεις δεν είναι ισοζυγισμένες.');
+      console.log(cnt);
+    }
+
+  });
 
 });
